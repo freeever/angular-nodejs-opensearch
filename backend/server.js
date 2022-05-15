d3dc2517da569743dc6d56405d07ce577598ca06
Daniel Xu <daniel.xu2@ontario.ca>
2022-05-14 22:59:55 -0400

    Corrected some issue related to Beanstalk deployment

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,17 @@
 const express = require("express");
+const cors = require("cors");
+const path = require('path');
+const fs = require('fs');
+
 const app = express();
 const bodyParser = require("body-parser");
-// parse requests of content-type - application/json
 app.use(bodyParser.json());
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
 writeENV();
 
-app.use(express.static(__dirname.replace(/\\/g, "/") + '/view/dist/frontend'))
+app.use(express.static(__dirname.replace(/\\/g, "/") + '/view/dist/frontend'));
 
-const cors = require('cors');
 app.use(cors())
 
 // simple route
@@ -21,12 +22,12 @@ app.get("/", (req, res) => {
 const routes = require("./app/route/geo.routes.js")(app);
 
 function writeENV() {
-  if (process.env.NODE_ENV) {
+if (process.env.NODE_ENV) {
       let content = "(function (window) {" +
           "window.__env = window.__env || {};" +
           "window.__env.SERVER_URL = '" + process.env.SERVER_URL + "';" +
           "}(this));"
-      fs.writeFile(path.join(__dirname.replace(/\\/g, "/"), '/view/dist/front/assets/environments/env.js'), content, (err) => {
+      fs.writeFile(path.join(__dirname.replace(/\\/g, "/"), '/view/dist/frontend/assets/environments/env.js'), content, (err) => {
           if (err) throw err;
           console.log('SERVER_URL :', process.env.SERVER_URL)
           console.log('Successfully saved env.js file.');
@@ -41,7 +42,7 @@ function sendSpaFileIfUnmatched(req, res) {
 }
 
 // set port, listen for requests
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
